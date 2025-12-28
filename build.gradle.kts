35dc1179a15bc55ed67222ee40a3243cfd673135
ganganbiz1 <y.iwamoto.0012.biz@gmail.com>
2025-12-28 12:19:48 +0900

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,11 +39,13 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
 }
 
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
 
